2faf89f21bb9e8c51783e15d733d5c2edca8685e
D-1183 <mustafaaltas3428@gmail.com>
2022-01-21 01:57:58 +0300

    JS calisma eklendi

---
+++ JS-21012022-var-type/script.js
@@ -0,0 +1,100 @@
+// console.log("ı'm from file");
+
+// var x = 7;
+// var y = 5;
+
+// console.log(x);
+// console.log({x});
+// console.log({y});
+
+// y=x;
+
+// console.log({y});
+
+
+
+
+
+
+
+// var a = "Global";
+// console.log(a);
+
+// {
+//     let a ="Scope";
+//     console.log(a);
+//     {
+//         a = "inner scope"
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+
+// console.log(a);
+
+
+
+// aşağıdaki kullanımda hata olmaz 
+// var d = 3
+// console.log(d);
+// var dd = 5;
+// console.log(d);
+
+
+// aşağıdaki kullanım hata verir
+// let c = 3;
+// console.log(c);
+// let c = 5;
+// console.log(c);
+
+
+// var f;
+// console.log(typeof f);
+
+// f = 5;
+// console.log(typeof f);
+
+// f = "String" ; 
+// console.log(typeof f);
+
+
+// console.log(typeof 5);
+// console.log(typeof 5.45);
+// console.log(typeof -2);
+// console.log(typeof 0);
+// console.log(typeof 0/1);
+// console.log(typeof NaN);
+// console.log(typeof (typeof 5));
+// console.log(typeof (1/0));
+// console.log(typeof typeof (1/0));
+
+
+// var merhaba = 'Merhaba';
+// var selam = "Selam";
+
+// var a = 'sana da selam """"""kjh"khg"hkbhk"j"';
+
+// var instructor = "Mustafa";
+// var d = `Merhaba ${instructor}`
+
+
+
+console.log(Boolean(""));
+console.log(Boolean(3 < 5));
+console.log(Boolean(3 > 5));
+console.log(Boolean(3 == 5));
+console.log(Boolean(0));
+console.log(Boolean("0"));
+var s;
+console.log(Boolean(s));
+console.log(Boolean(null));
+// console.log(typeof null); == > object
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log(2 == "2");
+console.log(2 === "2");
+
+console.log(2 + "2");
+console.log(2 ++ '2');
